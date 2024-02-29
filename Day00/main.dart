7e8b9e6ffc6adb034dadffbbb6663e816f3d2849
Nicolas <nicolas.lesage@hotmail.fr>
2024-02-29 19:03:23 +0100

    day00 ex00+ex01+ex02

---
+++ Day00/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyAppMat());
+}
+
+class MyAppMat extends StatelessWidget {
+  const MyAppMat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Ex00',
+      home: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: Colors.brown,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: (const Text(
+              'A simple text',
+              style: TextStyle(
+                color: Colors.white,
+                decoration: TextDecoration.none,
+                fontSize: 32,
+                fontWeight: FontWeight.normal,
+              ),
+            )),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print('Button pressed');
+            },
+            child: const Text('Press me'),
+          )
+        ],
+      ),
+    );
+  }
+}
