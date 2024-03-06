fd2e09c01584da08fb938e6ec59c5ccd095de038
Nicolas <nicolas.lesage@hotmail.fr>
2024-03-06 12:27:01 +0100

    using phone geolocation ok

--- Modulel02/weatherappv2_proj/lib/main.dart
+++ Modulel02/weatherappv2_proj/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-// import 'package:geolocator/geolocator.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
@@ -17,6 +19,7 @@ class _MainApp extends State<MainApp> {
   final myController = TextEditingController();
   String local = '';
   int _choice = 0;
+  bool _error = false;
 
   void setCity(String city) {
     setState(() {
@@ -24,34 +27,49 @@ class _MainApp extends State<MainApp> {
     });
   }
 
-  void setChoice(String choice) {
+  void setChoice(String choice) async {
     setState(() {
       if (choice == 'city') {
         _choice = 1;
+        _error = false;
+        print('choice set');
       } else if (choice == 'gps') {
         _choice = 2;
-        local = 'Geolocation';
+        // local = 'Geolocation';
 
         // trying to get localisation
-        // _getCurrentLocation();
-        // Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-        // print('position = $position');
+        _getCurrentLocation();
       } else {
         _choice = 0;
         local = '';
+        _error = false;
       }
     });
   }
 
-  // Future<void> _getCurrentLocation() async {
-  //   try {
-  //     Position position = await Geolocator.getCurrentPosition(
-  //         desiredAccuracy: LocationAccuracy.high);
-  //     print('position = $position');
-  //   } catch (e) {
-  //     print('Error getting current location: $e');
-  //   }
-  // }
+  Future<Position?> _getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      print('position = $position');
+      setState(() {
+        var latitude = position.latitude;
+        var longitude = position.longitude;
+
+        local = '$latitude $longitude';
+        _error = false;
+      });
+      return position;
+    } catch (e) {
+      print('Error getting current location: $e');
+      setState(() {
+        local =
+            'Geolocation is not available, please enable it in your App settings';
+        _error = true;
+      });
+      return null;
+    }
+  }
 
   @override
   void dispose() {
@@ -67,6 +85,7 @@ class _MainApp extends State<MainApp> {
         length: 3,
         child: Scaffold(
           appBar: AppBar(
+            backgroundColor: Colors.teal,
             title: MyAppBar(
               myController: myController,
               setCity: setCity,
@@ -74,9 +93,10 @@ class _MainApp extends State<MainApp> {
             ),
           ),
           body: TabBarView(children: [
-            MyTabWidget(title: 'Currently', localisation: local),
-            MyTabWidget(title: 'Today', localisation: local),
-            MyTabWidget(title: 'Weekly', localisation: local),
+            MyTabWidget(
+                title: 'Currently', localisation: local, isError: _error),
+            MyTabWidget(title: 'Today', localisation: local, isError: _error),
+            MyTabWidget(title: 'Weekly', localisation: local, isError: _error),
           ]),
           bottomNavigationBar: const MyBottonBarWidget(),
         ),
@@ -87,17 +107,26 @@ class _MainApp extends State<MainApp> {
 
 class MyTabWidget extends StatelessWidget {
   const MyTabWidget(
-      {super.key, required this.title, required this.localisation});
+      {super.key,
+      required this.title,
+      required this.localisation,
+      required this.isError});
 
   final String title;
   final String localisation;
+  final bool isError;
   // final TextEditingController myController;
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
-        if (constraints.minHeight > 40) {
-          return ShowResultsWidget(title: title, localisation: localisation);
+        if ((!isError && constraints.minHeight > 40) ||
+            constraints.minHeight > 60) {
+          return ShowResultsWidget(
+            title: title,
+            localisation: localisation,
+            isError: isError,
+          );
         } else {
           return Container();
         }
@@ -111,10 +140,12 @@ class ShowResultsWidget extends StatelessWidget {
     super.key,
     required this.title,
     required this.localisation,
+    required this.isError,
   });
 
   final String title;
   final String localisation;
+  final bool isError;
 
   @override
   Widget build(BuildContext context) {
@@ -126,9 +157,15 @@ class ShowResultsWidget extends StatelessWidget {
           title,
           style: const TextStyle(fontWeight: FontWeight.bold),
         ),
-        Text(
-          localisation,
-          style: const TextStyle(fontWeight: FontWeight.bold),
+        SingleChildScrollView(
+          child: Text(
+            localisation,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: isError ? Colors.red : Colors.black,
+            ),
+            textAlign: TextAlign.center,
+          ),
         ),
       ],
     );
@@ -194,9 +231,10 @@ class MyAppBar extends StatelessWidget {
               controller: myController,
               onChanged: (value) {
                 setCity(value);
+                setChoice('city');
               },
               decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
+                  // border: OutlineInputBorder(),
                   hintText: 'Localisation',
                   prefixIcon: Icon(Icons.search)),
             ),
