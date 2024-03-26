1854e9242993acf1a67e2f7e0b201e32e59c3959
Nicolas <nicolas.lesage@hotmail.fr>
2024-03-26 12:41:49 +0100

    today' view

--- Module03/weather_final_proj/lib/today.dart
+++ Module03/weather_final_proj/lib/today.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'class_def.dart';
 import 'decode_location.dart';
 import 'get_weather.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class TodayBody extends StatelessWidget {
   const TodayBody({
@@ -17,7 +19,7 @@ class TodayBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // print('TodayBody');
+    final screenHeight = MediaQuery.of(context).size.height;
     if (coord.latitude == 0) {
       return const Text('Please select a location');
     }
@@ -29,28 +31,213 @@ class TodayBody extends StatelessWidget {
           ] else
             const Text('No location data'),
           if (weather != null) ...[
-            ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 24,
-              itemBuilder: (context, index) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            SizedBox(height: screenHeight * 0.02),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.white.withOpacity(0.2),
+                ),
+                child: Column(
                   children: [
-                    Text(
-                        '${weather!.today!.time![index].hour.toString()}:${weather!.today!.time![index].minute.toString()}0'),
-                    Text('${weather!.today!.temperature![index].toString()}°C'),
-                    Text(
-                        '${weather!.today!.windSpeed10m![index].toString()} km/h'),
+                    const Text(
+                      'Today temperatures',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(
+                      height:
+                          screenHeight * 0.35 > 200 ? screenHeight * 0.35 : 200,
+                      // aspectRatio: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            right: 8, left: 5, top: 8, bottom: 5),
+                        child: LineChart(mainData()),
+                      ),
+                    ),
                   ],
-                );
-              },
+                ),
+              ),
+            ),
+            //space
+            SizedBox(height: screenHeight * 0.02),
+            // scroll infos
+            Container(
+              height: screenHeight * 0.22 > 110 ? screenHeight * 0.22 : 110,
+              width: double.infinity,
+              margin: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.white.withOpacity(0.2),
+              ),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  for (int i = 0; i < 24; i++) ...[
+                    SingleTodayElemWidget(weather: weather, i: i),
+                  ]
+                ],
+              ),
+              // SingleChildScrollView(
+              //   scrollDirection: Axis.horizontal,
+              //   child: Row(
+              //     children: [
+              //       for (int i = 0; i < 24; i++) ...[
+              //         SingleTodayElemWidget(weather: weather, i: i),
+              //       ]
+              //     ],
+              //   ),
+              // ),
             )
-            // Text('${weather!.current!.windSpeed10m.toString()} km/h'),
           ] else
             const Text('No weather data'),
         ],
       ),
+    );
+  }
+
+  Widget leftTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 15,
+    );
+    String text = "${value.toInt().toString()}°C";
+    return Text(text, style: style, textAlign: TextAlign.left);
+  }
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 15,
+    );
+    String text = "${value.toInt().toString()}:00";
+    if (text == '23:00') {
+      text = '';
+    }
+    return Text(text, style: style, textAlign: TextAlign.left);
+  }
+
+  LineChartData mainData() {
+    double minY = weather!.today!.temperature![0];
+    double maxY = weather!.today!.temperature![0];
+    for (int i = 1; i < 24; i++) {
+      if (minY > weather!.today!.temperature![i]) {
+        minY = weather!.today!.temperature![i];
+      }
+      if (maxY < weather!.today!.temperature![i]) {
+        maxY = weather!.today!.temperature![i];
+      }
+    }
+    minY = (minY - 2.0).ceilToDouble();
+    maxY = (maxY + 2.0).ceilToDouble();
+    return LineChartData(
+      gridData: FlGridData(
+        show: true,
+        drawVerticalLine: true,
+        // horizontalInterval: 3,
+        getDrawingHorizontalLine: (value) {
+          return const FlLine(
+            color: Colors.blueGrey,
+            strokeWidth: 1,
+          );
+        },
+        getDrawingVerticalLine: (value) {
+          return const FlLine(
+            color: Colors.blueGrey,
+            strokeWidth: 1,
+          );
+        },
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        rightTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        topTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            reservedSize: 30,
+            // interval: 3,
+            getTitlesWidget: bottomTitleWidgets,
+          ),
+        ),
+        leftTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            getTitlesWidget: leftTitleWidgets,
+            reservedSize: 34,
+          ),
+        ),
+      ),
+      minX: 0,
+      maxX: 23,
+      minY: minY,
+      maxY: maxY,
+      lineBarsData: [
+        LineChartBarData(
+          spots: [
+            for (int i = 0; i < 24; i++) ...[
+              FlSpot(i.toDouble(), weather!.today!.temperature![i]),
+            ]
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class SingleTodayElemWidget extends StatelessWidget {
+  const SingleTodayElemWidget({
+    super.key,
+    required this.weather,
+    required this.i,
+  });
+
+  final Weather? weather;
+  final int i;
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Column(
+      children: [
+        Text(
+          '${weather!.today!.time![i].hour.toString()}:00',
+          style: const TextStyle(
+            color: Colors.white,
+            // fontSize: 25,
+          ),
+        ),
+        SizedBox(height: screenHeight * 0.02),
+        // Text(weatherCodes[weather!.current!.weatherCode]!,
+        //     style: const TextStyle(color: Colors.white)),
+        // weatherIcon[weather!.current!.weatherCode]!,
+        getWeatherIcon(weather!.today!.weatherCode![i], screenHeight * 0.05),
+        Text(
+          '${weather!.today!.temperature![i].toString()}°C',
+          style: const TextStyle(
+            color: Colors.orange,
+            // fontSize: 25,
+          ),
+        ),
+        SizedBox(height: screenHeight * 0.02),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.air,
+              color: Colors.blue,
+              size: screenHeight * 0.02,
+            ),
+            Text(' ${weather!.today!.windSpeed10m![i].toString()} km/h',
+                style: const TextStyle(color: Colors.white)),
+          ],
+        ),
+      ],
     );
   }
 }
